58f111bfbededdd490ae34daedfdde7a6ccbd5d6
mati <mati@t221-pc13>
2018-05-16 12:18:52 +0200

    Cambios BgLabel

--- ArkhamProject/src/View/BgLabel.java
+++ ArkhamProject/src/View/BgLabel.java
@@ -6,9 +6,10 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-
 /**
- * Clase que actúa como un JLabel pero permite pintar un fondo mediante el Override del método paint
+ * Clase que actúa como un JLabel pero permite pintar un fondo mediante el
+ * Override del método paint
+ * 
  * @author elotrofausto
  *
  */
@@ -20,9 +21,10 @@ public class BgLabel extends JLabel {
 	public BgLabel(String ruta) {
 		tapiz = new ImageIcon(ruta).getImage();
 	}
-/**
- * Metodo para pintar el background del JLabel
- */
+
+	/**
+	 * Metodo para pintar el background del JLabel
+	 */
 	@Override
 	public void paint(Graphics g) {
 		int ancho = this.getSize().width;
@@ -30,15 +32,21 @@ public class BgLabel extends JLabel {
 		if (this.tapiz != null) {
 			g.drawImage(this.tapiz, 0, 0, ancho, alto, this);
 		}
-        this.setOpaque(false);
-        super.paint(g);
+		this.setOpaque(false);
+		super.paint(g);
 	}
-public Image getTapiz() {
-	return tapiz;
-}
-public void setTapiz(Image tapiz) {
-	this.tapiz = tapiz;
-}
+
+	public Image getTapiz() {
+		return tapiz;
+	}
+
+	public void setTapiz(Image tapiz) {
+		this.tapiz = tapiz;
+	}
 	
-	
+	@Override
+	public void update(Graphics g){
+		paint(g);
+	}
+
 }
